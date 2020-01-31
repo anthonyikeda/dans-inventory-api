b744965e94d9af240b2d126176372fe7ceb677c7
Anthony Ikeda <anthony.ikeda@gmail.com>
2020-01-31 14:37:19 +1100

    Setting up the Inventory API

---
+++ src/test/java/com/cloudyeng/service/inventoryapi/InventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudyeng.service.inventoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
